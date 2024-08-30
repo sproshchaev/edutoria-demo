f77f0a9f7697c489040f26f4b725240970bd8a3e
sproshchaev <sproshchaev@gmail.com>
2024-08-30 15:47:42 +0500

    upd

--- spring-boot-mapping-requests/task-02/task-02-solution/src/main/java/ru/edutoria/Main.java
+++ spring-boot-mapping-requests/task-02/task-02-solution/src/main/java/ru/edutoria/Main.java
@@ -1,10 +1,15 @@
 package ru.edutoria;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * task-02-solution
  */
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
+        SpringApplication.run(Main.class, args);
     }
 }
+
--- spring-boot-mapping-requests/task-03/task-03-solution/src/main/java/ru/edutoria/Main.java
+++ spring-boot-mapping-requests/task-03/task-03-solution/src/main/java/ru/edutoria/Main.java
@@ -1,9 +1,14 @@
 package ru.edutoria;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * task-03-solution
  */
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
